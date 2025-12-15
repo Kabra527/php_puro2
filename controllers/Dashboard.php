3fcb94270b7d18ba367f6d4073d249210a277a19
boo980718@gmail.com <rodriguezcristianleonardo8@gmail.com>
2025-12-14 21:20:08 -0500

    cambios

--- controllers/Dashboard.php
+++ controllers/Dashboard.php
@@ -1,9 +1,9 @@
 <?php
-class Dashboard{
-
-    public function main(){
+class Dashboard
+{
+    public function main()
+    {
         $session = $_SESSION['session'];
-        require_once "views/roles/".$session."/". $session.".view.php";
+        require_once "views/roles/" . $session . "/" . $session . ".view.php";
     }
 }
-?>
